ce39ca1352d26e9eec3afea176222a8f7cbd5a8e
ArmaanM <armaanmarathe@gmail.com>
2025-10-21 02:57:21 +0100

    Added the Basic Enemy data and some draft summoning
    Made the basic Enemy 3D Object with a new folder for materials - just fire there for now
    Enemy.cs is the enemy object
    EnemyData is for each individual spawned enemy
    EnemySummoner is the main script for summoning enemies

---
+++ Assets/Scripts/EnemySummoner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySummoner : MonoBehaviour
+{
+    public static List<Enemy> ExistingEnemies; //List of spawned, alive enemies
+    public static Dictionary<int, GameObject> EnemyPrefabs; //Components of EnemyData class, int is ID 
+    public static Dictionary<int, Queue<Enemy>> EnemyObjectPools; //Multiple enemy types need multiple queues
+    void Start()
+    {
+        EnemyPrefabs = new Dictionary<int, GameObject>();
+        EnemyObjectPools = new Dictionary<int, Queue<Enemy>>();
+        ExistingEnemies = new List<Enemy>();
+
+        EnemyData[] Enemies = Resources.LoadAll<EnemyData>("Enemies"); //Going through all directories until it reaches resource folders and puts a "\"
+
+        foreach (EnemyData enemy in Enemies)
+        {
+            EnemyPrefabs.Add(enemy.EnemyID, enemy.EnemyPrefab);
+            EnemyObjectPools.Add(enemy.EnemyID, new Queue<Enemy>());
+        }
+    }
+
+    public static Enemy SummonEnemy(int EnemyID)
+    {
+        Enemy SummonedEnemy = null;
+
+        if(EnemyPrefabs.ContainsKey(EnemyID)) //Check if enemyID exists by checking if prefab is stored with its ID in the prefabDictionary
+        {
+            Queue<Enemy> ReferencedQueue = EnemyObjectPools[EnemyID];
+
+            if(ReferencedQueue.Count > 0)
+            {
+                //Dequeue Enemy and init
+
+                SummonedEnemy = ReferencedQueue.Dequeue();
+                SummonedEnemy.init();
+            }
+            else
+            {
+                //Instantiate new instance of enemy and init
+                GameObject NewEnemy = Instantiate(EnemyPrefabs[EnemyID], Vector3.zero, Quaternion.identity);
+            }
+        }
+        else
+        {
+            Debug.Log($"Enemy with ID of {EnemyID} Does not exist");
+            return null;
+        }
+
+        return SummonedEnemy;
+    }
+    
+
+
+  
+}
