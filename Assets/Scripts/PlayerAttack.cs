696a31b2298ce59907f5134497f26bccf37ba818
Adam <adamornoch77@gmail.com>
2025-10-23 11:18:18 +0100

    Player targets closest single enemy
    - player will first enemy it comes in range until it dies, then it will move on to next enemy

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float timeToAttack = 2;
+    [SerializeField] private float damage = 2;
+    private Collider lockedOnEnemy;
+
     private float time;
 
     private void OnTriggerEnter(Collider other){
-        if (other.tag == "Enemy"){
+        if (other.tag == "Enemy" && lockedOnEnemy == null){
             time = 0;
+            lockedOnEnemy = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other == lockedOnEnemy){
+            lockedOnEnemy = null;
         }
     }
 
     private void OnTriggerStay(Collider other){
         if (other.tag == "Enemy"){
-            if (time <= 0){
-                //Attack
-                Debug.Log("DAMAGE");
-                time = timeToAttack;
-            }else{
-                time -= Time.deltaTime;
+            if (other == lockedOnEnemy){
+                if (time <= 0){
+                    //Attack
+                    Debug.Log(lockedOnEnemy);
+                    time = timeToAttack;
+                }else{
+                    time -= Time.deltaTime;
+                }
+            } else if (lockedOnEnemy == null){
+                lockedOnEnemy = other;
             }
         }
     }
