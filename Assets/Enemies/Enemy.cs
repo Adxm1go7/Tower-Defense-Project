8b40868596975c5864fcd5d80c13886e587448a6
ArmaanM <armaanmarathe@gmail.com>
2025-10-23 22:26:15 +0100

    Fixing merge conflicts

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    public int NodeIndex;
     public int ID;
     public float MaxHealth;
     public float Health;
@@ -16,25 +15,16 @@ public class Enemy : MonoBehaviour
     public void init()
     {
         Health = MaxHealth;
-        NodeIndex = 0;
-
-        if (Nodes.nodesArray.Length > 0)
-        {
-            transform.position = Nodes.nodesArray[0].position;
-            if (Nodes.nodesArray.Length > 1)
-                targetNode = Nodes.nodesArray[1];
-
-        }
     }
 
     public void TakeDamage(int damage)
     {
         Health -= damage;
-        if (Health <=0)
+        if (Health <= 0)
         {
             Destroy(this.gameObject);
-        } 
+        }
     }
-    
+
 
 }
