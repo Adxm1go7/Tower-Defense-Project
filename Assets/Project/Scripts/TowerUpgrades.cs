d90dc27d305c5b1d16e02e53de971b557323b3d6
Adam Ornoch <Adamigo77@gmail.com>
2026-01-09 01:44:07 +0000

    Revert "Fix"
    This reverts commit 727f5476a28eb877c82ea159efa69b2507facc4b, reversing
    changes made to b23a90e0791945b3034c118f965b614121c211c4.

--- Assets/Project/Scripts/TowerUpgrades.cs
+++ Assets/Project/Scripts/TowerUpgrades.cs
@@ -181,16 +181,17 @@ public class TowerUpgrades : MonoBehaviour
             {
                 if (path == "A")
                 {
-                    investor.incomeData.timeInterval -= 0.75f;
+                    investor.incomeData.timeInterval -= 0.7f;
                 }
                 else if (path == "B")
                 {
-                    investor.incomeData.coinsGenerated += 1;
+                    investor.incomeData.coinsGenerated += 2;
                     investor.incomeData.maxStoredCoins += 10;
                     
                 }
 
             }
+            investor.incomeData.maxStoredCoins += 25;
             //Debug.Log(investor.incomeData.timeInterval);
             //Debug.Log(investor.incomeData.coinsGenerated);
         }
